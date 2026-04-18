c6f5bae4f634133d64bf6c31caa1ada685f0323c
mudumbaisankeerth-prog <mudumbaisankeerth@gmail.com>
2026-04-17 22:25:00 -0500

    212

---
+++ Utils/preprocessing.py
@@ -0,0 +1,26 @@
+# Preprocessing 
+
+# Imports
+import numpy
+import pandas
+import seaborn
+
+#################################################
+
+# Preprocessing class
+class Preprocessing():
+    def __init__(self, df):
+        pass
+        self.df = df
+
+    def train_test_split():
+        pass
+    
+    def normalize():
+        pass 
+
+    def standardize():
+        pass
+
+
+
